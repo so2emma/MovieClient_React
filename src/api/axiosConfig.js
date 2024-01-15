743c2c6774a9c84931815baa045d5f4171c91ee0
so2emma <osoemmanuel1969@gmail.com>
2024-01-15 23:04:49 +0100

    receiving data from spring application

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export default axios.get({
-    baseURL: 'http://localhost:8080',
-    headers: {"ngrok-skip-browser-warning": "true"}
+export default axios.create({
+ baseURL: "http://localhost:8080",
+ headers: {
+   "Content-type": "application/json"
+ }
 });
